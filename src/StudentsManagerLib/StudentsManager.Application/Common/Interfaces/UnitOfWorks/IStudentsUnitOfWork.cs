cbc38b814d1aa76f62f0e3ab4dbaef395bf6adee
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-09-20 11:13:35 +0300

    Добавлен UnitOfWork для работы с несколькими репозиториями и поддержки транзакции

---
+++ src/StudentsManagerLib/StudentsManager.Application/Common/Interfaces/UnitOfWorks/IStudentsUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using StudentsManager.Application.Common.Interfaces.Repositories;
+using StudentsManager.Application.Common.Interfaces.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsManager.Application.Common.Interfaces.UnitOfWorks
+{
+    public interface IStudentsUnitOfWork: ITransactable
+    {
+        ICuratorRepository CuratorRepository { get; }
+        IGroupRepository GroupRepository { get; }
+        IPersonRepository PersonRepository { get; }
+        ISpecialtyRepository SpecialtyRepository { get; }
+        IStudentRepository StudentRepository { get; }
+    }
+}
---
+++ src/StudentsManagerLib/StudentsManager.Infrastructure/UnitOfWorks/StudentsUnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using StudentsManager.Application.Common.Interfaces.Repositories;
+using StudentsManager.Application.Common.Interfaces.Transactions;
+using StudentsManager.Application.Common.Interfaces.UnitOfWorks;
+using StudentsManager.Infrastructure.DbContexts;
+using StudentsManager.Infrastructure.Repositories;
+
+namespace StudentsManager.Infrastructure.UnitOfWorks
+{
+    internal class StudentsUnitOfWork : IStudentsUnitOfWork, IDisposable
+    {
+        private readonly StudentsDbContext _context;
+        ICuratorRepository? _curatorRepository;
+        IGroupRepository? _groupRepository;
+        IPersonRepository? _personRepository;
+        ISpecialtyRepository? _specialtyRepository;
+        IStudentRepository? _studentRepository;
+
+        public StudentsUnitOfWork(StudentsDbContext context) 
+        {
+            _context = context;
+        }
+        public ICuratorRepository CuratorRepository => _curatorRepository ?? (_curatorRepository = new CuratorRepository(_context));
+
+        public IGroupRepository GroupRepository => _groupRepository ?? (_groupRepository = new GroupRepository(_context));
+
+        public IPersonRepository PersonRepository => _personRepository ?? (_personRepository = new PersonRepository(_context));
+
+        public ISpecialtyRepository SpecialtyRepository => _specialtyRepository ?? (_specialtyRepository = new SpecialtyRepository(_context));
+
+        public IStudentRepository StudentRepository => _studentRepository ?? (_studentRepository = new StudentRepository(_context));
+
+        public async Task<ITransaction> BeginTransactionAsync()
+        {
+            return new Transaction(await _context.Database.BeginTransactionAsync());
+        }
+        public void Dispose()
+        {
+            _context?.Dispose();
+        }
+    }
+}
