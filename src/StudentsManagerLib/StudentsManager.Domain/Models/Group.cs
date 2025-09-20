61fb117f33f4cd24de1f867ba315178a3ddaf7b6
Daniil Grabovsky <56531440+ArBiTheG@users.noreply.github.com>
2025-09-20 12:02:38 +0300

    #1 - изменены модели на базовые свойства

--- src/StudentsManagerLib/StudentsManager.Domain/Models/Group.cs
+++ src/StudentsManagerLib/StudentsManager.Domain/Models/Group.cs
@@ -4,5 +4,12 @@ namespace StudentsManager.Domain.Models
 {
     public class Group: BaseEntity
     {
+        public int SpecialtyId { get; set; }
+        public Specialty Specialty { get; set; } = null!;
+
+        public int? CuratorId { get; set; }
+        public Curator? Curator { get; set; }
+
+        public ICollection<Student> Students { get; set; } = new List<Student>();
     }
 }
--- src/StudentsManagerLib/StudentsManager.Domain/Models/Specialty.cs
+++ src/StudentsManagerLib/StudentsManager.Domain/Models/Specialty.cs
@@ -4,5 +4,8 @@ namespace StudentsManager.Domain.Models
 {
     public class Specialty: BaseEntity
     {
+        public string Code { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public ICollection<Group> Groups { get; set; } = new List<Group>();
     }
 }
